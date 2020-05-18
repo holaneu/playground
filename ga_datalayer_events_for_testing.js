53ed75058ed9ae851d2f2a9f139ec124728a8ca1
holaneu <20854147+holaneu@users.noreply.github.com>
2020-05-18 22:51:04 +0200

    Update ga_datalayer_events_for_testing.js

--- ga_datalayer_events_for_testing.js
+++ ga_datalayer_events_for_testing.js
@@ -1,7 +1,47 @@
 /** simple datalayer push event generator */
-var eventList = 'cartAdd,download,ecommerceTransaction,filter,login,outbound_link,productClick,productDetailView,promotionClick,promotionView,registerConsultant,search,share,sort,test_event,userRegistered,userUpgraded';
-var events = eventList.split(',');
-events.forEach(function(item, index) { window.dataLayer.push({'event' : item}); });
+var eventList = [
+    'begin_checkout',
+    'cartAdd',
+    'checkout_progress',
+    'download',
+    'ecommerceTransaction',
+    'filter',
+    'login',
+    'outbound_link',
+    'productClick',
+    'productDetailView',
+    'productImpression',
+    'promotionClick',
+    'promotionView',
+    'registerConsultant',
+    'remove_from_cart',
+    'search',
+    'share',
+    'sort',
+    'test_event',
+    'userRegistered',
+    'userUpgraded',
+    'virtualPageView'
+];
+
+/* pushes with interval timer */
+function dlp2() {
+    var index = 0;
+    var timer = setInterval(pusher, 1100);
+    function pusher() {
+      if (index == eventList.length) {
+        clearInterval(timer);
+      } else {        
+        window.dataLayer.push({ 'event' : eventList[index] });
+        index++;
+      }
+    }
+  }
+
+ dlp2();
+
+ /* pushes without interval timer */
+ eventList.forEach(function(item, index) { window.dataLayer.push({'event' : item}); });
 
 
 /* dlp = 'datalayer push' */
@@ -30,7 +70,5 @@ dlp('filter',{'filter_type' : 'brand', 'filter_value' : 'Giordani'});
 dlp('test_event');
 dlp('ecommerceTransaction');
 
-
-
-
-
+/* manual datalayer pushes */
+window.dataLayer.push({'event' : 'ecommerceTransaction'});
