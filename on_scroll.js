1eb7f365f7233f3a954d1fe175f982291e3c9192
holaneu <20854147+holaneu@users.noreply.github.com>
2023-04-06 09:19:06 +0200

    Delete on_scroll.js

--- on_scroll.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("myP").className = "test";
-  } else {
-    document.getElementById("myP").className = "";
-  }
-}
