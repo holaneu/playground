1b69212b5921e53b38551db8cc8c7ecdcdf85987
holaneu <20854147+holaneu@users.noreply.github.com>
2023-04-12 13:56:29 +0200

    Update test_cron_2.py

--- test_cron_2.py
+++ test_cron_2.py
@@ -7,8 +7,8 @@ now = datetime.now()
 formatted_date = now.strftime('%Y%m%d%H%M%S')
 
 print(f"Current working directory: {working_directory}")
-print(f"File path: {path}"
+print(f"File path: {path}")
 print(f"Formatted date: {formatted_date}")
 
 with open(path, 'a', encoding='utf-8') as f:
-    f.write(formatted_date + '\n')
+    f.write('origin:root, ' + formatted_date + '\n')
