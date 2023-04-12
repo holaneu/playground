bce73bc4c58f5756c86b118821808df444647e85
holaneu <20854147+holaneu@users.noreply.github.com>
2023-04-12 11:27:46 +0200

    Update test_cron.py

--- _python/test_cron.py
+++ _python/test_cron.py
@@ -1,7 +1,12 @@
 from datetime import datetime
+import os
+
+print(f"Current working directory: {os.getcwd()}")
 
 now = datetime.now()
 formatted_date = now.strftime('%Y%m%d%H%M%S')
+
+print(f"Formatted date: {formatted_date}")
 
 with open('_python/test_cron_files/test_cron.txt', 'a', encoding="utf-8") as f:
     f.write(formatted_date + '\n')
